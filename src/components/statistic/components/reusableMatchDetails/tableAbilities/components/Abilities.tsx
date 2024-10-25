924d917627597a2bb12e25a8bb28dc70dcf760eb
BazyshynAnton <maybebazishin@gmail.com>
2024-10-25 16:31:13 +0300

    ability tooltip

--- src/components/statistic/components/reusableMatchDetails/tableAbilities/components/Abilities.tsx
+++ src/components/statistic/components/reusableMatchDetails/tableAbilities/components/Abilities.tsx
@@ -7,6 +7,12 @@ import { Image } from "@/shared/nextjsImports"
 import { Player } from "@/types/statistic/tableDetails"
 
 import styles from "@/styles/statistic/TableAbilities.module.scss"
+import AbilityDescription from "./abilityDescription/AbilityDescription"
+import { useEffect, useState } from "react"
+
+type IsToolTip = {
+  [key: string]: boolean
+}
 
 export default function Abilities({
   player,
@@ -15,6 +21,8 @@ export default function Abilities({
   player: Player
   uRowDetails: PlayerRowDetailsUtility
 }) {
+  const [isTooltip, setIsTooltip] = useState<IsToolTip[]>()
+
   const abilityBuild = uRowDetails.setAbilityBuild(player.ability_upgrades_arr)
 
   return (
@@ -22,19 +30,47 @@ export default function Abilities({
       {abilityBuild.map((ability, idx) => {
         const abilityName = uRowDetails.findAbilityByID(ability)
 
+        setIsTooltip((prevState: any) => {
+          const newState = { ...prevState, [abilityName]: false }
+
+          return newState
+        })
+
         const talentTree: boolean = abilityName.includes("special_bonus")
+
+        const handleMouseEnter = () => {
+          setIsTooltip((prevState: any) => {
+            const newState = { ...prevState, [idx]: true }
+
+            return newState
+          })
+        }
+        const handleMouseLeave = () => {
+          setIsTooltip((prevState: any) => {
+            const newState = { ...prevState, [idx]: false }
+
+            return newState
+          })
+        }
+
+        if (!isTooltip) return
 
         return (
           <td key={idx} className={styles.tableBodyRow__abilityDataCell}>
             <div className={styles.abilityDataCell__inCell}>
               {abilityName !== "none" && (
                 <>
+                  {isTooltip[idx] && (
+                    <AbilityDescription abilityName={abilityName} />
+                  )}
                   {!talentTree ? (
                     <Image
                       src={`${HERO_ABILITY_URL}${abilityName}.png`}
                       alt=""
                       width={100}
                       height={100}
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}
                     />
                   ) : (
                     <Image
--- src/components/statistic/components/reusableMatchDetails/tableAbilities/components/abilityDescription/AbilityDescription.tsx
+++ src/components/statistic/components/reusableMatchDetails/tableAbilities/components/abilityDescription/AbilityDescription.tsx
@@ -1,15 +1,23 @@
+import Title from "./components/Title"
+
 import { ReactDOM } from "@/shared/reactImports"
 
 import styles from "@/styles/statistic/AbilityDescription.module.scss"
 
-export default function ItemDescription({}) {
+export default function AbilityDescription({
+  abilityName,
+}: {
+  abilityName: string
+}) {
   //
   /* 
     Using React Portal transfer this component to another
     component with id="tooltip_portal"
   */
   return ReactDOM.createPortal(
-    <div className={styles.abilityTooltip}></div>,
+    <div className={styles.abilityTooltip}>
+      <Title />
+    </div>,
     document.getElementById("tooltip_portal") as Element | DocumentFragment
   )
 }
