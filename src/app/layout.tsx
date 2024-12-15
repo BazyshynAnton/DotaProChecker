56c242b5bf8e7e663484c8a8bc7325353a20bf83
BazyshynAnton <maybebazishin@gmail.com>
2024-12-15 16:28:23 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
-import TooltipItemPortal from "@/components/portals/TooltipItemPortal";
-import TooltipAbilityPortal from "@/components/portals/TooltipAbilityPortal";
+import Header from "@/components/header/Header"
+import Footer from "@/components/footer/Footer"
+import TooltipItemPortal from "@/components/portals/TooltipItemPortal"
+import TooltipAbilityPortal from "@/components/portals/TooltipAbilityPortal"
 
-import StoreProvider from "@/store/StoreProvider";
+import StoreProvider from "@/store/StoreProvider"
 
-import { Exo_2 } from "next/font/google";
+import { Exo_2 } from "next/font/google"
 
-import type { Metadata } from "next";
+import type { Metadata } from "next"
 
-import "@/styles/globals.scss";
+import "@/styles/globals.scss"
 
 const exo2 = Exo_2({
   weight: ["400", "500", "700", "900"],
   style: ["normal"],
   subsets: ["latin"],
-});
+})
 
-export   const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "DotaScope",
-  //icons: {
-  //   apple: ["/favicon/apple-touch-icon.png?v=4"],
-  //   icon: ["/favicon/favicon.ico?v=4"],
-  //   shortcut: ["/favicon/apple-touch-icon.png"],
-  // },
-};
+  icons: {
+    apple: ["/favicon/apple-touch-icon.png?v=4"],
+    icon: ["/favicon/favicon.ico?v=4"],
+    shortcut: ["/favicon/apple-touch-icon.png"],
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <StoreProvider>
@@ -45,5 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </StoreProvider>
-  );
+  )
 }
