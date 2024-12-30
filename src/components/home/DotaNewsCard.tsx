f51d855ed5d0a5ce834568a41115277bad95c52c
BazyshynAnton <maybebazishin@gmail.com>
2024-12-30 16:31:20 +0200

    swiper

--- src/components/home/DotaNewsCard.tsx
+++ src/components/home/DotaNewsCard.tsx
@@ -3,9 +3,14 @@ import { NewsItem } from "@/types/home/homeDataUtility"
 
 export default function DotaNewsCard({ newsItem }: { newsItem: NewsItem }) {
   const date = new Date(newsItem.date * 1000)
+  let formattedTitle =
+    newsItem.title.length > 25
+      ? newsItem.title.substring(0, 24) + "..."
+      : newsItem.title
+
   return (
     <a href={newsItem.url} className={styles.dotaNewsCard} target="_blank">
-      <h4>{newsItem.title}</h4>
+      <h4>{formattedTitle}</h4>
       <div className={styles.newsDetails}>
         <p>{newsItem.feedlabel}</p>
         <p>Author: {newsItem.author}</p>
--- src/components/home/InteractiveList.tsx
+++ src/components/home/InteractiveList.tsx
@@ -3,8 +3,12 @@ import DotaNewsCard from "./DotaNewsCard"
 import ContentHeader from "./ContentHeader"
 import StatisticLoader from "../loaders/StatisticLoader"
 
+import { useEffect, useState } from "@/shared/reactImports"
 import { useAppSelector } from "@/hooks/useAppSelector"
+import { Swiper, SwiperSlide } from "swiper/react"
 
+import "swiper/css"
+import "swiper/css/navigation"
 import styles from "@/styles/home/Home.module.scss"
 
 export default function InteractiveList({
@@ -17,32 +21,60 @@ export default function InteractiveList({
   const { proMatches, dotaNews } = useAppSelector((store) => store.homeSlice)
   const news = dotaNews?.appnews.newsitems
 
+  const [isDesktop, setIsDesktop] = useState(false)
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsDesktop(window.innerWidth >= 1190)
+
+      const handleResizeEvent = () => {
+        setIsDesktop(window.innerWidth >= 1190)
+      }
+
+      window.addEventListener("resize", handleResizeEvent)
+      return () => {
+        window.removeEventListener("resize", handleResizeEvent)
+      }
+    }
+  }, [])
+
   return (
     <div
       className={type === "matchesList" ? styles.proMatches : styles.dotaNews}
     >
       <ContentHeader headerTitle={listHeader} />
-      <div
-        className={
-          type === "matchesList"
-            ? styles.proMatches__content
-            : styles.dotaNews__content
-        }
-      >
-        {type === "matchesList" ? (
-          proMatches ? (
-            proMatches.map((match) => (
-              <ProMatchCard key={match.match_id} proMatch={match} />
+      <Swiper slidesPerView={3} grabCursor={true}>
+        <div
+          className={
+            type === "matchesList"
+              ? styles.proMatches__content
+              : styles.dotaNews__content
+          }
+        >
+          {type === "matchesList" ? (
+            proMatches ? (
+              proMatches.map((match) =>
+                isDesktop ? (
+                  <ProMatchCard key={match.match_id} proMatch={match} />
+                ) : (
+                  <SwiperSlide key={match.match_id}>
+                    <ProMatchCard proMatch={match} />
+                  </SwiperSlide>
+                ),
+              )
+            ) : (
+              <Loader />
+            )
+          ) : news ? (
+            news.map((el) => (
+              <SwiperSlide key={el.gid}>
+                <DotaNewsCard newsItem={el} />
+              </SwiperSlide>
             ))
           ) : (
             <Loader />
-          )
-        ) : news ? (
-          news.map((el) => <DotaNewsCard key={el.gid} newsItem={el} />)
-        ) : (
-          <Loader />
-        )}
-      </div>
+          )}
+        </div>
+      </Swiper>
     </div>
   )
 }
@@ -61,3 +93,7 @@ function Loader() {
     </div>
   )
 }
+
+/**
+ * 1. no pag, nav
+ */
